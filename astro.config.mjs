99c3adefc87d33d820bb902efcdb95f152e27075
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2023-01-21 11:07:55 +0100

    Add robots.txt generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://mandaljazz.github.io",
-  integrations: [tailwind(), sitemap(), prefetch()],
+  integrations: [tailwind(), sitemap(), prefetch(), robotsTxt()]
 });
