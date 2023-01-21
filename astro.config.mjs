338438535c907013d804fd449972930d1e2b6561
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2023-01-21 10:35:50 +0100

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from "astro/config";
-
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mandaljazz.github.io",
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
 });
