069931624f5d922b30d533eadd32007557315e0f
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2023-02-16 23:04:31 +0100

    Add color theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#8bc5ff",
+        "dark-blue": "#390099",
+        indigo: "#9e0059",
+        orange: "#ff5400",
+        pink: "#ff0054",
+        yellow: "#ffbd00",
+        black: "#121212",
+        white: "#f1f1f1",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
