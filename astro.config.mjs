ebd8a03a1fefd42c4a1410381097d9097c71b4f9
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2023-02-16 22:11:30 +0100

    Revert "Merge pull request #2 from mandaljazz/setup-firebase"
    This reverts commit 01f8846b6e632ea798a9fb101128b576ae139ef0, reversing
    changes made to 7adc1d825b15bd7b7b2d4ca0c158861f54d720df.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import firebase from "astro-firebase";
+
+// https://astro.build/config
 import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mandaljazz.github.io",
-  integrations: [tailwind(), sitemap(), prefetch(), robotsTxt(), mdx()],
-  adapter: firebase(),
+  integrations: [tailwind(), sitemap(), prefetch(), robotsTxt(), mdx()]
 });
