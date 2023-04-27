a89c82c44c64df4490b149481364431c37aa2247
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2023-04-27 18:02:57 +0200

    Add small versions of hands

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,3 +1,7 @@
 export const randomHandSrc = `/profil/hand0${Math.floor(
   Math.random() * (4 - 1) + 1
 )}.png`;
+
+export const randomSmallHandSrc = `/profil/hand0${Math.floor(
+  Math.random() * (4 - 1) + 1
+)}-cut.png`;
