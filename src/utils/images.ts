9e30a6562d0675a66c1a8325883e433783a628e3
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2023-06-21 19:26:13 +0200

    Make image random at runtime, not just build time

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,7 +1,5 @@
-export const randomHandSrc = `/profil/hand0${Math.floor(
-  Math.random() * (4 - 1) + 1
-)}.png`;
+export const randomHandSrc = () =>
+  `/profil/hand0${Math.floor(Math.random() * (4 - 1) + 1)}.png`;
 
-export const randomSmallHandSrc = `/profil/hand0${Math.floor(
-  Math.random() * (4 - 1) + 1
-)}-cut.png`;
+export const randomSmallHandSrc = () =>
+  `/profil/hand0${Math.floor(Math.random() * (4 - 1) + 1)}-cut.png`;
