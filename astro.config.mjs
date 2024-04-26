7b8cc0434c56e5c584899630cb9d01dc56a430f7
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2024-04-26 13:30:50 +0200

    Use built in prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,13 @@
-import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import lottie from "astro-integration-lottie";
-
-// https://astro.build/config
 import robotsTxt from "astro-robots-txt";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mandaljazz.no/",
-  integrations: [
-    tailwind(),
-    sitemap(),
-    prefetch(),
-    robotsTxt(),
-    mdx(),
-    lottie(),
-  ],
+  prefetch: true,
+  integrations: [tailwind(), sitemap(), robotsTxt(), mdx(), lottie()],
 });
