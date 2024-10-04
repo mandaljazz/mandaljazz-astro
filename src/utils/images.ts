65798414ec5e854a6b5441139363cddb16da6d72
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2024-10-04 17:09:33 +0200

    Start using Image component for better image optimization

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,5 +1,21 @@
+import type { ImageMetadata } from "astro";
+
 export const randomHandSrc = () =>
   `/profil/hand0${Math.floor(Math.random() * (4 - 1) + 1)}.png`;
 
 export const randomSmallHandSrc = () =>
   `/profil/hand0${Math.floor(Math.random() * (4 - 1) + 1)}-cut.png`;
+
+export const imagePathToSrc = (imagePath: string) => {
+  const images = import.meta.glob<{ default: ImageMetadata }>(
+    "/src/images/**/*.{jpeg,jpg,png,gif}",
+  );
+
+  if (!images[imagePath]) {
+    throw new Error(
+      `"${imagePath}" does not exist in glob: "src/images/*.{jpeg,jpg,png,gif}"`,
+    );
+  }
+
+  return images[imagePath]();
+};
