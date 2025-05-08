bb3b1ea76662c20991557111044016c13086f70f
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2025-05-08 23:08:18 +0200

    feat: add privacy focused umami tracking

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,20 @@ import lottie from "astro-integration-lottie";
 import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
 
+import yeskunallumami from "@yeskunall/astro-umami";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://mandaljazz.no/",
   prefetch: true,
-  integrations: [tailwind(), sitemap(), robotsTxt(), mdx(), lottie()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    robotsTxt(),
+    mdx(),
+    lottie(),
+    yeskunallumami({ id: "d43a3a76-eba4-47c6-b19f-8c6499479a8b" }),
+  ],
   image: {
     domains: ["live.staticflickr.com"],
   },
