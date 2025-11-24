8c917fe1938d69865f36ec7379c47783092e45ad
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2025-11-24 22:26:45 +0100

    feat: keep blog posts as content instead

---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+import { defineCollection, z } from "astro:content";
+
+const blogg = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    author: z.string().optional(),
+    authorImage: z.string().optional(),
+    ingress: z.string().optional(),
+    image: z.string(),
+    createdAt: z.string().transform((str) => new Date(str)),
+    updatedAt: z
+      .union([z.string().transform((str) => new Date(str)), z.undefined()])
+      .optional(),
+    hide: z.boolean().default(false),
+  }),
+});
+
+export const collections = {
+  blogg,
+};
