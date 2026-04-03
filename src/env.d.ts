f5950174affca9af23b35b51b1eaf62af4c8a2cf
Hans Henrik Grønsleth <hei@hanshenrik.com>
2026-04-03 20:20:15 +0200

    feat: make map more intereseting

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_MAPBOX_ACCESS_TOKEN?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
